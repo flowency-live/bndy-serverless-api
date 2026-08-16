ca714c8955b29d5e058b01b1de0c3eb07ef21490
Flowency <jason@flowency.co.uk>
2026-08-16 03:14:52 +0100

    Mock Google place details in venue dedupe tests

--- venues-lambda/geocode-dedupe.test.js
+++ venues-lambda/geocode-dedupe.test.js
@@ -20,6 +20,17 @@ const mockDynamoDB = {
 
 const mockPlacesClient = {
   findPlaceFromText: jest.fn(),
+  placeDetails: jest.fn().mockResolvedValue({
+    data: {
+      status: 'OK',
+      result: {
+        types: ['bar'],
+        address_components: [
+          { long_name: 'ST1 1AA', types: ['postal_code'] }
+        ]
+      }
+    }
+  })
 };
 
 jest.mock('aws-sdk', () => ({
